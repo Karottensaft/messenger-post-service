261b7105e0adae22e64264d6e8d8cb5965168cbe
Karottensaft <Karottensaft.Rus@gmail.com>
2022-08-31 19:06:45 +1000

    swaggerwithocelot-working

--- PostMessengerService.WebAPI/Program.cs
+++ PostMessengerService.WebAPI/Program.cs
@@ -1,10 +1,39 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PostMessengerService.Application.Middlewares;
 using PostMessengerService.Application.Services;
 using PostMessengerService.Infrastructure.Data;
 using PostMessengerService.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
